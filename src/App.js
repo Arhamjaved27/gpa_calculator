4f3127c38933668115201cbdea808afe9e294121
Arham <arhamjaved867@gmail.com>
2024-09-22 20:50:22 +0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+
+      <div className="container-fluid bg-info" style={{ height: '90vh' }} >
+        <div className="bg-success  h-100 p-5">
+          <label for="exampleColorInput" className="form-label">Color picker</label>
+          <input type="color" className="form-control form-control-color" id="exampleColorInput" value="#563d7c" title="Choose your color" />
+
+
+          <div className="mt-2 mb-2 input-group">
+
+            <label className="btn btn-danger mt-2" htmlFor="inputGroupSelect01">Number Of Books...</label>
+            <select className="form-select p-1 m-2" id="inputGroupSelect01">
+              <option selected>0</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="3">4</option>
+              <option value="3">5</option>
+              <option value="3">6</option>
+              <option value="3">7</option>
+              <option value="3">8</option>
+              <option value="3">9</option>
+            </select>
+          </div>
+
+          <table className="table table-dark">
+            <thead>
+              <tr>
+                <th scope="col">Books</th>
+                <th scope="col">Credit hours</th>
+                <th scope="col">Grade</th>
+                <th scope="col">Grade Points</th>
+                <th scope="col">Individual GPA</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@sdf</td>
+              </tr>
+
+            </tbody>
+          </table>
+          
+
+
+
+        </div>
+      </div>
+
+    </>
   );
 }
 
